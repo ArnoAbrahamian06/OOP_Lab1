77d85302ae1285f77cbeaeee0c0df73f1b9b202a
abramyanarno2006 <abramyanarno2006@gmail.com>
2025-09-14 23:18:34 +0400

    Добавление решения четветого задания

---
+++ task 4/MyFirstProgram.java
@@ -0,0 +1,16 @@
+import myfirstpackage.*;
+
+class MyFirstClass {
+    public static void main(String[] s) {
+        MyFirstPackage o = new MyFirstPackage(0, 0);
+        System.out.println(o.subtract());
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j <= 8; j++) {
+                o.setA(i);
+                o.setB(j);
+                System.out.print(o.subtract() + " ");
+            }
+            System.out.println();
+        }
+    }
+}
